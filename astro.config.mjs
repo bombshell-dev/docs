b390c158ab89b03584e2d69cc0754814e30122b1
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2025-03-21 14:31:33 -0600

    add: initial documentation structure and baseline content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,49 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import ecTwoSlash from "expressive-code-twoslash";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Bombshell",
+      customCss: [
+        // Relative path to your @font-face CSS file.
+        "./src/fonts/font-face.css",
+      ],
+      logo: {
+        dark: "./src/assets/dark.svg",
+        light: "./src/assets/light.svg",
+      },
+      components: {
+        Head: "./src/starlightOverrides/Head.astro",
+      },
+      expressiveCode: {
+        plugins: [ecTwoSlash()],
+      },
+      editLink: {
+        baseUrl: "https://github.com/bombshell-dev/clack/docs/edit/main/",
+      },
+      social: {
+        discord: "https://bomb.sh/chat",
+        blueSky: "https://bomb.sh/on/bluesky",
+        github: "https://bomb.sh/on/github",
+      },
+      sidebar: [
+        {
+          label: "Basics",
+          autogenerate: { directory: "basics" },
+        },
+        {
+          label: "Clack",
+          autogenerate: { directory: "clack" },
+        },
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+      ],
+    }),
+  ],
 });
