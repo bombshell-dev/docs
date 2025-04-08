1d6f77cbeaf78cfb1146d1b1c1a09ea001d92c44
Nate Moore <natemoo-re@users.noreply.github.com>
2025-04-07 22:46:12 -0500

    Add topics (#6)
    * feat: add topics plugin
    
    * chore: update site URL
    
    * chore: format
    
    * feat: remove args

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,84 +1,97 @@
 import starlight from "@astrojs/starlight";
 // @ts-check
 import { defineConfig } from "astro/config";
-import ecTwoSlash from "expressive-code-twoslash";
 
-const site = "https://docs.bomb.sh/";
+import ecTwoSlash from "expressive-code-twoslash";
+import topics from "starlight-sidebar-topics";
+
+const site = "https://bomb.sh/docs/";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://docs.bomb.sh/',
-  base: '/docs',
-  outDir: './dist/docs/',
-  integrations: [
-    starlight({
-      title: "Bombshell",
-      customCss: [
-        // Relative path to your @font-face CSS file.
-        "./src/fonts/font-face.css",
-      ],
-      logo: {
-        dark: "./src/assets/dark.svg",
-        light: "./src/assets/light.svg",
-      },
-      components: {
-        Head: "./src/starlightOverrides/Head.astro",
-      },
-      expressiveCode: {
-        plugins: [ecTwoSlash()],
-      },
-      editLink: {
-        baseUrl: "https://github.com/bombshell-dev/clack/docs/edit/main/",
-      },
-      head: [
-        {
-          tag: "meta",
-          attrs: {
-            name: "og:image",
-            content: `${site}og-docs.png`,
-          },
-        },
-        {
-          tag: "meta",
-          attrs: {
-            name: "twitter:image",
-            content: `${site}og-docs.png`,
-          },
-        },
-        {
-          tag: "meta",
-          attrs: {
-            name: "twitter:site",
-            content: "bombshell",
-          },
-        },
-        {
-          tag: "meta",
-          attrs: {
-            name: "twitter:creator",
-            content: "bombshell",
-          },
-        },
-      ],
-      social: {
-        discord: "https://bomb.sh/chat",
-        blueSky: "https://bomb.sh/on/bluesky",
-        github: "https://bomb.sh/on/github",
-      },
-      sidebar: [
-        {
-          label: "Basics",
-          autogenerate: { directory: "basics" },
-        },
-        {
-          label: "Clack",
-          autogenerate: { directory: "clack" },
-        },
-        {
-          label: "Guides",
-          autogenerate: { directory: "guides" },
-        },
-      ],
-    }),
-  ],
+	site: "https://bomb.sh/",
+	base: "/docs",
+	outDir: "./dist/docs/",
+	integrations: [
+		starlight({
+			title: "Bombshell",
+			customCss: [
+				// Relative path to your @font-face CSS file.
+				"./src/fonts/font-face.css",
+			],
+			logo: {
+				dark: "./src/assets/dark.svg",
+				light: "./src/assets/light.svg",
+			},
+			components: {
+				Head: "./src/starlightOverrides/Head.astro",
+			},
+			expressiveCode: {
+				plugins: [ecTwoSlash()],
+			},
+			editLink: {
+				baseUrl: "https://github.com/bombshell-dev/docs/edit/main/",
+			},
+			head: [
+				{
+					tag: "link",
+					attrs: {
+						rel: "stylesheet",
+						href: "https://use.typekit.net/bst3mzh.css?v=4",
+					},
+				},
+				{
+					tag: "meta",
+					attrs: {
+						name: "og:image",
+						content: `${site}og-docs.png`,
+					},
+				},
+				{
+					tag: "meta",
+					attrs: {
+						name: "twitter:image",
+						content: `${site}og-docs.png`,
+					},
+				},
+				{
+					tag: "meta",
+					attrs: {
+						name: "twitter:site",
+						content: "bombshell",
+					},
+				},
+				{
+					tag: "meta",
+					attrs: {
+						name: "twitter:creator",
+						content: "bombshell",
+					},
+				},
+			],
+			social: {
+				discord: "https://bomb.sh/chat",
+				blueSky: "https://bomb.sh/on/bluesky",
+				github: "https://bomb.sh/on/github",
+			},
+			plugins: [
+				topics([
+					{
+						label: "Clack",
+						id: "clack",
+						icon: "seti:hex",
+						link: "/clack/basics/getting-started",
+						items: [
+							{ label: "Basics", autogenerate: { directory: "clack/basics" } },
+							{
+								label: "Packages",
+								autogenerate: { directory: "clack/packages" },
+							},
+							{ label: "Guides", autogenerate: { directory: "clack/guides" } },
+						],
+					}
+				]),
+			],
+		}),
+	],
 });
