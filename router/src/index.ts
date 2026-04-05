4093cb5371e7670d653890b8ad0c8b9be46846d7
Nate Moore <nate@natemoo.re>
2026-04-04 21:48:42 -0400

    fix(coep): move _headers

--- router/src/index.ts
+++ router/src/index.ts
@@ -1,24 +1,20 @@
 export interface Env {}
 
-const EXTRA_HEADERS: [string, string][] = [
-	["Cross-Origin-Embedder-Policy", "require-corp"],
-	["Cross-Origin-Opener-Policy", "same-origin"],
-];
-
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
 
 		if (url.pathname.startsWith("/docs")) {
 			const response = await fetch(new URL(url.pathname, "https://docs.bomb.sh/"));
-			const headers = new Headers([...response.headers, ...EXTRA_HEADERS]);
+			const headers = new Headers(response.headers);
+			headers.set("Cross-Origin-Embedder-Policy", "require-corp");
+			headers.set("Cross-Origin-Opener-Policy", "same-origin");
 			return new Response(response.body, {
 				status: response.status,
 				statusText: response.statusText,
 				headers,
 			});
 		}
-		
 
 		return fetch(url);
 	},
