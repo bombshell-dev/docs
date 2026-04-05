5724155e422581f6ee40873889290216165c2b78
Nate Moore <nate@natemoo.re>
2026-04-04 22:22:05 -0400

    ref(router): redirect to subdomain

--- router/src/index.ts
+++ router/src/index.ts
@@ -1,5 +1,11 @@
 export interface Env {}
 
+const HEADERS = {
+	"Cross-Origin-Embedder-Policy": "require-corp",
+	"Cross-Origin-Opener-Policy": "same-origin",
+	"Cross-Origin-Resource-Policy": "cross-origin"
+}
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
@@ -7,9 +13,9 @@ export default {
 		if (url.pathname.startsWith("/docs")) {
 			const response = await fetch(new URL(url.pathname, "https://docs.bomb.sh/"));
 			const headers = new Headers(response.headers);
-			headers.set("Cross-Origin-Embedder-Policy", "require-corp");
-			headers.set("Cross-Origin-Opener-Policy", "same-origin");
-			headers.set("Cross-Origin-Resource-Policy", "cross-origin");
+			for (const [name, value] of Object.entries(HEADERS)) {
+				headers.set(name, value);
+			}
 			return new Response(response.body, {
 				status: response.status,
 				statusText: response.statusText,
@@ -17,6 +23,6 @@ export default {
 			});
 		}
 
-		return fetch(url);
+		return Response.redirect(`https://bomb.sh/docs${url.pathname}`, 301);
 	},
 } satisfies ExportedHandler<Env>;
